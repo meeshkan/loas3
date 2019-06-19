4660dcd10009d8f3304abbd45600fc021d45fdf8
Mike Solomon <mike@meeshkan.com>
2019-06-19 07:14:10 +0300

    Adds more tests

---
+++ test/model/oai/reference.test.ts
@@ -0,0 +1,22 @@
+import { ReferenceObject } from "../../../src/model/oai/reference";
+
+test("reference object cannot be empty", () => {
+  expect(
+    ReferenceObject.is({})
+  ).toBe(false);
+});
+test("reference object cannot contain extra fields", () => {
+  expect(
+    ReferenceObject.is({
+      $ref: "Idan",
+      "x-foo": 1
+    })
+  ).toBe(false);
+});
+test("reference object must contain $ref", () => {
+  expect(
+    ReferenceObject.is({
+      $ref: "Kimmo"
+    })
+  ).toBe(true);
+});
---
+++ test/model/oai/xml.test.ts
@@ -0,0 +1,31 @@
+import { XMLObject } from "../../../src/model/oai/xml";
+
+test("contact object validates", () => {
+  expect(
+    XMLObject.is({
+      name: "a",
+      prefix: "b"
+    })
+  ).toBe(true);
+  expect(
+    XMLObject.is({})
+  ).toBe(true);
+  expect(
+    XMLObject.is({
+      name: "Mike",
+      prefix: 1
+    })
+  ).toBe(false);
+  expect(
+    XMLObject.is({
+      name: "Mike",
+      foo: "1"
+    })
+  ).toBe(false);
+  expect(
+    XMLObject.is({
+      name: "Mike",
+      "x-foo": "1"
+    })
+  ).toBe(true);
+});
