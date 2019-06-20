7d8f1518ddf8e762048a48dbb2929535cfadd12f
Mike Solomon <mike@meeshkan.com>
2019-06-20 22:48:36 +0300

    Adds lazy files

---
+++ test/schema/lazy-happy-path.test.ts
@@ -0,0 +1,31 @@
+import fs from "fs";
+import yaml from "js-yaml";
+import { Validator } from "jsonschema";
+
+test("schema validates without operations specified", () => {
+  var v = new Validator();
+  var instance = yaml.load(fs.readFileSync("./test/schema/pet-store-uses-get-default.yml").toString());
+  var schema = yaml.load(fs.readFileSync("./schema/lazy.yml").toString())
+  expect(v.validate(instance, schema).errors.length).toBe(0);
+});
+
+test("schema validates without operations or resposnes specified", () => {
+  var v = new Validator();
+  var instance = yaml.load(fs.readFileSync("./test/schema/pet-store-uses-responses-default.yml").toString());
+  var schema = yaml.load(fs.readFileSync("./schema/lazy.yml").toString())
+  expect(v.validate(instance, schema).errors.length).toBe(0);
+})
+
+test("schema validates without operations, resposnes or response specified", () => {
+  var v = new Validator();
+  var instance = yaml.load(fs.readFileSync("./test/schema/pet-store-uses-response-default.yml").toString());
+  var schema = yaml.load(fs.readFileSync("./schema/lazy.yml").toString())
+  expect(v.validate(instance, schema).errors.length).toBe(0);
+})
+
+test("schema validates without operations, resposnes or response specified", () => {
+  var v = new Validator();
+  var instance = yaml.load(fs.readFileSync("./test/schema/pet-store-uses-ref-default.yml").toString());
+  var schema = yaml.load(fs.readFileSync("./schema/lazy.yml").toString())
+  expect(v.validate(instance, schema).errors.length).toBe(0);
+})
