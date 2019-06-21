263c0b44361bb6183890a8f6a96bd56d5d8cea12
Mike Solomon <mike@meeshkan.com>
2019-06-21 06:55:44 +0300

    Uses tv4

--- test/schema/lazy-happy-path.test.ts
+++ test/schema/lazy-happy-path.test.ts
@@ -1,54 +1,49 @@
 import fs from "fs";
 import yaml from "js-yaml";
-import { Validator } from "jsonschema";
+import tv4 from "tv4";
 
 test("full schema validates", () => {
-  const v = new Validator();
   const instance = yaml.load(
     fs.readFileSync("./test/schema/pet-store-full.yml").toString()
   );
   const lazy = yaml.load(fs.readFileSync("./schema/lazy.yml").toString());
   const full = yaml.load(fs.readFileSync("./schema/full.yml").toString());
-  expect(v.validate(instance, lazy).errors.length).toBe(0);
-  expect(v.validate(instance, full).errors.length).toBe(0);
+  expect(tv4.validate(instance, lazy)).toBe(true);
+  expect(tv4.validate(instance, full)).toBe(true);
 });
 
 test("schema validates without operations specified", () => {
-  const v = new Validator();
   const instance = yaml.load(
     fs.readFileSync("./test/schema/pet-store-uses-get-default.yml").toString()
   );
   const schema = yaml.load(fs.readFileSync("./schema/lazy.yml").toString());
-  expect(v.validate(instance, schema).errors.length).toBe(0);
+  expect(tv4.validate(instance, schema)).toBe(true);
 });
 
 test("schema validates without operations or resposnes specified", () => {
-  const v = new Validator();
   const instance = yaml.load(
     fs
       .readFileSync("./test/schema/pet-store-uses-responses-default.yml")
       .toString()
   );
   const schema = yaml.load(fs.readFileSync("./schema/lazy.yml").toString());
-  expect(v.validate(instance, schema).errors.length).toBe(0);
+  expect(tv4.validate(instance, schema)).toBe(true);
 });
 
 test("schema validates without operations, resposnes or response specified", () => {
-  const v = new Validator();
   const instance = yaml.load(
     fs
       .readFileSync("./test/schema/pet-store-uses-response-default.yml")
       .toString()
   );
   const schema = yaml.load(fs.readFileSync("./schema/lazy.yml").toString());
-  expect(v.validate(instance, schema).errors.length).toBe(0);
+  expect(tv4.validate(instance, schema)).toBe(true);
 });
 
 test("schema validates without operations, resposnes or response specified", () => {
-  const v = new Validator();
   const instance = yaml.load(
     fs.readFileSync("./test/schema/pet-store-uses-ref-default.yml").toString()
   );
   const schema = yaml.load(fs.readFileSync("./schema/lazy.yml").toString());
-  expect(v.validate(instance, schema).errors.length).toBe(0);
+  expect(tv4.validate(instance, schema)).toBe(true);
 });
