4fa7e572a9b633d75701d26a68ceb76475f14fb0
Mike Solomon <mike@meeshkan.com>
2019-06-23 17:54:29 +0300

    Fixes path

--- src/lazy/parameter.ts
+++ src/lazy/parameter.ts
@@ -1,6 +1,47 @@
 import { $LazyLazyParams, $LazyParams } from "../generated/lazy";
 import { Parameter } from "../generated/full";
+import _schema from "./schema";
 
-export const _lazylazy = (o: $LazyLazyParams): Parameter[] => [];
+export const _lazylazy = (o: $LazyLazyParams, path: string): Parameter[] =>
+  Object.entries(o).map(([a, b]) => ({
+    name: a,
+    // if the name is foo, heck if {foo} the path, if not theere, default to query
+    in: path.indexOf(`{${a}}`) !== -1 ? "path" : "query",
+    schema: _schema(b)
+  }));
 
-export const _lazy = (o: $LazyParams): Parameter[] => [];
+export const _lazy = ({
+  query,
+  header,
+  cookie,
+  path
+}: $LazyParams): Parameter[] => [
+  ...(query
+    ? Object.entries(query).map(([name, v]) => ({
+        in: "query",
+        name,
+        schema: _schema(v)
+      }))
+    : []),
+  ...(header
+    ? Object.entries(header).map(([name, v]) => ({
+        in: "header",
+        name,
+        schema: _schema(v)
+      }))
+    : []),
+  ...(path
+    ? Object.entries(path).map(([name, v]) => ({
+        in: "path",
+        name,
+        schema: _schema(v)
+      }))
+    : []),
+  ...(cookie
+    ? Object.entries(cookie).map(([name, v]) => ({
+        in: "cookie",
+        name,
+        schema: _schema(v)
+      }))
+    : [])
+];
--- src/lazy/parameters.ts
+++ src/lazy/parameters.ts
@@ -5,35 +5,37 @@ import {
   is$LazyLazyParams,
   is$LazyParams,
   is$Parameter,
-  is$Schema
+  is$Schema,
+  $Schema,
+  $LazyLazyParams
 } from "../generated/lazy";
 import { Parameter, Reference } from "../generated/full";
 import { _lazylazy, _lazy } from "./parameter";
 import _mediaType from "./mediaType";
 import _schema from "./schema";
 
+const __ = ({ schema, content, ...rest }: $Parameter) => ({
+  ...rest,
+  ...(content
+    ? {
+        content: Object.entries(content)
+          .map(([a, b]) => ({ [a]: _mediaType(b) }))
+          .reduce((a, b) => ({ ...a, ...b }), {})
+      }
+    : {}),
+  ...(schema
+    ? {
+        schema: is$Schema(schema) ? _schema(schema) : schema
+      }
+    : {})
+});
+
 export default (
-  o:
-    | $LazyParams
-    | Record<string, Record<string, string | number | boolean>>
-    | ($Reference | $Parameter)[]
+  o: $LazyParams | $LazyLazyParams | ($Reference | $Parameter)[],
+  path: string
 ): (Reference | Parameter)[] =>
   is$LazyLazyParams(o)
-    ? _lazylazy(o)
+    ? _lazylazy(o, path)
     : is$LazyParams(o)
     ? _lazy(o)
-    : o.filter(is$Parameter).map(({ schema, content, ...rest }) => ({
-        ...rest,
-        ...(content
-          ? {
-              content: Object.entries(content)
-                .map(([a, b]) => ({ [a]: _mediaType(b) }))
-                .reduce((a, b) => ({ ...a, ...b }), {})
-            }
-          : {}),
-        ...(schema
-          ? {
-              schema: is$Schema(schema) ? _schema(schema) : schema
-            }
-          : {})
-      }));
+    : o.map(i => (is$Parameter(i) ? __(i) : i));
