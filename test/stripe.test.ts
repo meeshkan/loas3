ad2a830fdbbf8ec2b035c9624328e772ae9201bf
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-07-30 15:01:53 +0300

    Clean the util in tests by renaming and using fold instead of isLeft checks. (#2)
    * Clean the util in tests by renaming and using fold.
    
    * Remove extra import.

--- test/stripe.test.ts
+++ test/stripe.test.ts
@@ -1,11 +1,12 @@
 import fs from "fs";
 import yaml from "js-yaml";
 import loas from "../src";
-import { lazy } from "./util";
+import { mapRightOrThrow } from "./util";
 
 test("stripe in, stripe out", () => {
-  const instance = yaml.load(fs.readFileSync("./test/stripe.yml").toString());
-  lazy(loas(instance), val => {
-    expect(val).toEqual(instance);
+  const spec = yaml.load(fs.readFileSync("./test/stripe.yml").toString());
+  const expandedSpecOrErrors = loas(spec);
+  mapRightOrThrow(expandedSpecOrErrors, val => {
+    expect(val).toEqual(spec);
   });
 });
