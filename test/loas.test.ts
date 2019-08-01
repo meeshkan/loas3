8a8e9c253e83c9202b9060f5331c5d145a14b242
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-01 13:08:39 +0300

    Add a failing test for response .

---
+++ test/loas.test.ts
@@ -0,0 +1,42 @@
+import loas from "../src";
+import { mapRightOrThrow } from "./util";
+
+const specWithResponseRef = {
+  paths: {
+    "/users": {
+      options: {
+        responses: {
+          204: {
+            $ref: "#/components/responses/Options"
+          }
+        }
+      }
+    }
+  },
+  components: {
+    responses: {
+      Options: {
+        description: "Options response",
+        content: {
+          "text/plain": {
+            schema: {
+              type: "string"
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+describe("loas", () => {
+  it("should handle response object ref correctly", () => {
+    const expandedSpecOrErrors = loas(specWithResponseRef);
+    mapRightOrThrow(expandedSpecOrErrors, spec => {
+      expect(spec).toHaveProperty(
+        ["paths", "/users", "options", "responses", 204, "description"],
+        "Options response"
+      );
+    });
+  });
+});
