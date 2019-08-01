ac9a137f0e13dcd62519f51e39e145fdf49cd9bc
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-01 12:53:58 +0300

    Add loadYaml helper in test utils. (#7)
    * Add helper for loading yml.
    
    * Use loadYaml throughout.
    
    * Remove extra import.

--- test/util.ts
+++ test/util.ts
@@ -1,6 +1,8 @@
 import { Either, fold } from "fp-ts/lib/Either";
 import { ErrorObject } from "ajv";
 import { OpenAPIObject } from "../src/generated/full";
+import yaml from "js-yaml";
+import fs from "fs";
 
 export const mapRightOrThrow = (
   res: Either<ErrorObject[], OpenAPIObject>,
@@ -9,4 +11,8 @@ export const mapRightOrThrow = (
   return fold((errs: ErrorObject[]) => {
     throw new Error(errs.map(i => i.message).join("\n"));
   }, f)(res);
+};
+
+export const loadYaml = (pathToFile: string): object => {
+  return yaml.load(fs.readFileSync(pathToFile, "utf-8"));
 };
